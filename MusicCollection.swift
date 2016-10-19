9d4badfa20e59fbf0aaf9ed3460ceed381bae7c1
goolhanrry <879316283@qq.com>
2016-10-19 22:59:01 +0800

    Create MusicCollection.swift

---
+++ MusicCollection.swift
@@ -0,0 +1,16 @@
+//
+//  MusicCollection.swift
+//  Swift Template
+//
+//  Created by goolhanrry on 10/14/16.
+//  Copyright © 2016 ZQ-iOS-Tech-Group. All rights reserved.
+//
+
+import Foundation
+
+class MusicCollection {
+    class Library {
+        var library:Class = Song
+    }
+    class Playlist {}
+}
