29af868d1445ba377b3164410a8204ec91888664
goolhanrry <879316283@qq.com>
2016-10-19 22:55:58 +0800

    Create song.swift

---
+++ song.swift
@@ -0,0 +1,39 @@
+//
+//  Song.swift
+//  Swift Template
+//
+//  Created by goolhanrry on 10/14/16.
+//  Copyright © 2016 ZQ-iOS-Tech-Group. All rights reserved.
+//
+
+import Foundation
+
+class Song {
+    class Song1 {
+        let title = Song1
+        let artist = artist_a
+        let album = album_a
+        let playing_time:Double = 2.00
+    }
+
+    class Song2{
+        let title = Song2
+        let artist = artist_b
+        let album = album_b
+        let playing_time:Double = 2.34
+    }
+
+    class Song3{
+        let title = Song3
+        let artist = artist_b
+        let album = album_c
+        let playing_time:Double = 2.12
+    }
+
+    class Song4{
+        let title = Song4
+        let artist = artist_c
+        let album = album_d
+        let playing_time:Double = 1.57
+    }
+}
