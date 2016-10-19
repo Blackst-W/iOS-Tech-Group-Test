53902e50d4bd2444f498dae958a709f1c2779585
goolhanrry <879316283@qq.com>
2016-10-19 22:57:59 +0800

    Create playlist.swift

---
+++ playlist.swift
@@ -0,0 +1,18 @@
+//
+//  Playlist.swift
+//  Swift Template
+//
+//  Created by goolhanrry on 10/14/16.
+//  Copyright © 2016 ZQ-iOS-Tech-Group. All rights reserved.
+//
+
+import Foundation
+
+class Playlist {
+    class Playlist1 {
+        var playlist1:Class = [Song1,Song2]
+    }
+    class Playlist2 {
+        var playlist2:Class = [Song3,Song4]
+    }
+}
